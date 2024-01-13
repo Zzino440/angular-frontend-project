7c185802aa2ef4970b127fd03565205034242d9f
Lorenzo Belli <l.belli@dstech.it>
2024-01-13 14:31:04 +0100

    fix env

--- src/app/features/employee/services/employee.service.ts
+++ src/app/features/employee/services/employee.service.ts
@@ -2,24 +2,22 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {EmployeeModel} from "../models/employee.model";
+import {environment} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
-  private baseURL: string = "http://localhost:8080/api/v1/"
+  private environment = environment.endpointUri
 
   constructor(private httpClient: HttpClient) {
-
   }
 
   getEmployeeList(): Observable<EmployeeModel[]> {
-    return this.httpClient.get<EmployeeModel[]>(`${this.baseURL}employees`)
+    return this.httpClient.get<EmployeeModel[]>(`${this.environment}employees`)
   }
 
   createEmployee(employee: EmployeeModel): Observable<Object> {
-    return this.httpClient.post(`${this.baseURL}employees`, employee)
-
+    return this.httpClient.post(`${this.environment}employees`, employee)
   }
 }
