685289f7aa451fba74a1e08f9451024553e0802b
Lorenzo Belli <l.belli@dstech.it>
2024-01-14 02:28:34 +0100

    fix

--- src/app/shared/directives/prevent-numbers.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PreventNumbersDirective } from './prevent-numbers.directive';
-
-describe('PreventNumbersDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PreventNumbersDirective();
-    expect(directive).toBeTruthy();
-  });
-});
