f645cb21e077d63cc40ee1d8532e276adb20b9dc
Lorenzo Belli <l.belli@dstech.it>
2024-01-19 16:31:00 +0100

    changed method to get users and put in the fe table - using async pipe

--- src/app/features/user/pages/user-list/user-list.component.ts
+++ src/app/features/user/pages/user-list/user-list.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {User} from "../../models/user";
-import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {MatTableModule} from "@angular/material/table";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
 import {MatDialog} from "@angular/material/dialog";
 import {DeleteUserDialogComponent} from "../../components/delete-user-dialog/delete-user-dialog.component";
-import {Subscription} from "rxjs";
+import {Observable} from "rxjs";
+import {AsyncPipe} from "@angular/common";
+import {DatasourcePipe} from "../../../../shared/pipes/datasource.pipe";
 
 @Component({
   selector: 'app-user-list',
@@ -16,54 +18,43 @@ import {Subscription} from "rxjs";
     MatTableModule,
     MatButtonModule,
     MatIconModule,
-    RouterLink
+    RouterLink,
+    AsyncPipe,
+    DatasourcePipe
   ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent implements OnInit, OnDestroy {
 
+  userList$!: Observable<User[]>;
+
   //utils variables
-  private subscription: Subscription = new Subscription();
-
-  userList!: User[];
-
   displayedColumns: string[] = ['firstName', 'lastName', 'email', 'actions']
-
-  datasource: MatTableDataSource<User> = new MatTableDataSource<User>();
 
   constructor(private userService: UserService, public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
-    this.getUsers()
+    this.getUsers();
   }
 
   getUsers() {
-    this.subscription.add(
-      this.userService.getUserList().subscribe(res => {
-        this.userList = res;
-        this.datasource.data = res;
-      })
-    )
+    this.userList$ = this.userService.getUserList();
   }
 
   openDeleteUserDialog(userId: number) {
     const dialogRef = this.dialog.open(DeleteUserDialogComponent, {
       data: {userId: userId}
     });
-
-    this.subscription.add(
-      dialogRef.afterClosed().subscribe(result => {
-        if (result?.status === 'success') {
-          this.getUsers();
-        } else if (result?.status === 'cancelled') {
-        }
-      })
-    );
+    dialogRef.afterClosed().subscribe(result => {
+      if (result?.status === 'success') {
+        this.getUsers();
+      } else if (result?.status === 'cancelled') {
+      }
+    })
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 }
