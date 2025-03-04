b1c7b24e637a0c2330a5db65203ceadbad000a64
Zzino <lorenzo97belli@gmail.com>
2025-03-04 19:59:49 +0100

    refactor new user addition component for improved code readability and structure

--- src/app/features/user/pages/new-user-add/new-user-add.component.ts
+++ src/app/features/user/pages/new-user-add/new-user-add.component.ts
@@ -20,160 +20,162 @@ import {SnackBarNotificationService} from "../../../../shared/services/snack-bar
 import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
-    selector: 'app-new-user-add',
-    standalone: true,
-    imports: [
-        MatCard,
-        ReactiveFormsModule,
-        MatFormField,
-        MatLabel,
-        MatInput,
-        PreventNumbersDirective,
-        MatButton,
-        MatSelect,
-        MatOption,
-        MatError,
-        MatIcon,
-        MatProgressSpinner,
-        MatSuffix,
-        NgIf
-    ],
-    templateUrl: './new-user-add.component.html',
-    styleUrl: './new-user-add.component.scss'
+  selector: 'app-new-user-add',
+  standalone: true,
+  imports: [
+    MatCard,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    PreventNumbersDirective,
+    MatButton,
+    MatSelect,
+    MatOption,
+    MatError,
+    MatIcon,
+    MatProgressSpinner,
+    MatSuffix,
+    NgIf
+  ],
+  templateUrl: './new-user-add.component.html',
+  styleUrl: './new-user-add.component.scss'
 })
 export class NewUserAddComponent implements OnInit, OnDestroy {
 
-    formBuilder = inject(FormBuilder);
-    activatedRoute = inject(ActivatedRoute);
-    userService = inject(UserService);
-    customValidators = inject(CustomValidators);
-    snackBarNotificationService = inject(SnackBarNotificationService);
-    router = inject(Router);
+  formBuilder = inject(FormBuilder);
+  activatedRoute = inject(ActivatedRoute);
+  userService = inject(UserService);
+  customValidators = inject(CustomValidators);
+  snackBarNotificationService = inject(SnackBarNotificationService);
+  router = inject(Router);
 
-    //main variables
-    private user = signal<User>(new User());
+  //main variables
+  private user = signal<User>(new User());
 
-    userForm = this.formBuilder.nonNullable.group({
-        firstName: new FormControl({value: '', disabled: false}, {
-            validators: [Validators.required, this.customValidators.lettersOnlyValidator],
-            nonNullable: true
-        }),
-        lastName: new FormControl({value: '', disabled: false}, {
-            validators: [Validators.required, this.customValidators.lettersOnlyValidator],
-            nonNullable: true
-        }),
-        email: new FormControl({value: '', disabled: false}, {
-            validators: [Validators.required, Validators.email],
-            asyncValidators: [this.customValidators.emailExistsValidator()],
-            updateOn: 'blur',
-            nonNullable: true
-        }),
-        password: new FormControl({value: '', disabled: false}, {
-            validators: [Validators.required],
-            nonNullable: true
-        }),
-        role: new FormControl({value: '', disabled: false}, {
-            validators: [Validators.required],
-            nonNullable: true
-        })
+  userForm = this.formBuilder.nonNullable.group({
+    firstName: new FormControl({value: '', disabled: false}, {
+      validators: [Validators.required, this.customValidators.lettersOnlyValidator],
+      nonNullable: true
+    }),
+    lastName: new FormControl({value: '', disabled: false}, {
+      validators: [Validators.required, this.customValidators.lettersOnlyValidator],
+      nonNullable: true
+    }),
+    email: new FormControl({value: '', disabled: false}, {
+      validators: [Validators.required, Validators.email],
+      asyncValidators: [this.customValidators.emailExistsValidator()],
+      updateOn: 'blur',
+      nonNullable: true
+    }),
+    password: new FormControl({value: '', disabled: false}, {
+      validators: [Validators.required],
+      nonNullable: true
+    }),
+    role: new FormControl({value: '', disabled: false}, {
+      validators: [Validators.required],
+      nonNullable: true
+    })
+  });
+
+  //utility variables
+  currentUserId = signal<number>(0);
+  isEditUser = computed(() => this.currentUserId() > 0);
+
+  roleOptions = Object.values(Role);
+
+  protected readonly destroy$ = new Subject<void>();
+
+  constructor() {
+    this.activatedRoute.paramMap
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(params => {
+        this.currentUserId.set(Number(params.get('id')));
+      });
+  }
+
+  ngOnInit(): void {
+    if (this.isEditUser()) {
+      this.setFormValuesAndValidatorsAndState();
+    }
+  }
+
+  public submitForm() {
+    this.isEditUser() ? this.updateUser() : this.addUser();
+  }
+
+  private setFormValuesAndValidatorsAndState() {
+
+    this.passwordControl.clearValidators();
+    this.passwordControl.updateValueAndValidity();
+    this.emailControl.clearAsyncValidators();
+    this.emailControl.updateValueAndValidity();
+
+    this.userService.getUserById(this.currentUserId()).pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(user => {
+      this.user.set(user);
+      this.userForm.patchValue(this.user());
     });
+  }
 
-    //utility variables
-    currentUserId = signal<number>(0);
-    isEditUser = computed(() => this.currentUserId() > 0);
+  addUser() {
+    this.userService.createUser(this.userForm.value).subscribe({
+      next: res => {
+        console.log('res save', res);
+        this.snackBarNotificationService.notify('User created successfully', 'OK', NotificationTypeEnum.SUCCESS);
+        this.goToUserList();
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('err during the save ', err);
+      }
+    })
+  }
 
-    roleOptions = Object.values(Role);
+  updateUser() {
+    this.userService.updateUser(this.currentUserId(), this.userForm.value).subscribe({
+      next: res => {
+        console.log('res update', res)
+        this.snackBarNotificationService.notify('User updated successfully', 'OK', NotificationTypeEnum.SUCCESS);
+        this.goToUserList();
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('err during the update ', err);
+      }
+    })
+  }
 
-    protected readonly destroy$ = new Subject<void>();
+  goToUserList() {
+    this.router.navigate(['/users']).then();
+  }
 
-    constructor() {
-        this.activatedRoute.paramMap.subscribe(params => {
-            this.currentUserId.set(Number(params.get('id')));
-        })
-    }
+  get firstNameControl() {
+    return this.userForm.controls.firstName;
+  }
 
-    ngOnInit(): void {
-        if (this.isEditUser()) {
-            this.setFormValuesAndValidatorsAndState();
-        }
-    }
+  get lastNameControl() {
+    return this.userForm.controls.lastName;
+  }
 
-    public submitForm() {
-        this.isEditUser() ? this.updateUser() : this.addUser();
-    }
+  get emailControl() {
+    return this.userForm.controls.email;
+  }
 
-    private setFormValuesAndValidatorsAndState() {
+  get passwordControl() {
+    return this.userForm.controls.password;
+  }
 
-        this.passwordControl.clearValidators();
-        this.passwordControl.updateValueAndValidity();
-        this.emailControl.clearAsyncValidators();
-        this.emailControl.updateValueAndValidity();
-
-        this.userService.getUserById(this.currentUserId()).pipe(
-            takeUntil(this.destroy$)
-        ).subscribe(user => {
-            this.user.set(user);
-            this.userForm.patchValue(this.user());
-        });
-    }
-
-    addUser() {
-        this.userService.createUser(this.userForm.value).subscribe({
-            next: res => {
-                console.log('res save', res);
-                this.snackBarNotificationService.notify('User created successfully', 'OK', NotificationTypeEnum.SUCCESS);
-                this.goToUserList();
-            },
-            error: (err: HttpErrorResponse) => {
-                console.log('err during the save ', err);
-            }
-        })
-    }
-
-    updateUser() {
-        this.userService.updateUser(this.currentUserId(), this.userForm.value).subscribe({
-            next: res => {
-                console.log('res update', res)
-                this.snackBarNotificationService.notify('User updated successfully', 'OK', NotificationTypeEnum.SUCCESS);
-                this.goToUserList();
-            },
-            error: (err: HttpErrorResponse) => {
-                console.log('err during the update ', err);
-            }
-        })
-    }
-
-    goToUserList() {
-        this.router.navigate(['/users']).then();
-    }
-
-    get firstNameControl() {
-        return this.userForm.controls.firstName;
-    }
-
-    get lastNameControl() {
-        return this.userForm.controls.lastName;
-    }
-
-    get emailControl() {
-        return this.userForm.controls.email;
-    }
-
-    get passwordControl() {
-        return this.userForm.controls.password;
-    }
-
-    get roleControl() {
-        return this.userForm.controls.role;
-    }
+  get roleControl() {
+    return this.userForm.controls.role;
+  }
 
 
-    reset() {
-        this.userForm.reset(this.user());
-    }
+  reset() {
+    this.userForm.reset(this.user());
+  }
 
-    ngOnDestroy() {
-        this.destroy$.next();
-        this.destroy$.complete();
-    }
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
